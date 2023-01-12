2129a2a645977906c7a3c9f2b7da2d0ebe2acd57
Thanh Nam Tran <ttran19@umbc.edu>
2023-01-12 00:21:34 -0500

    configure post controllers

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,68 @@ import './App.css';
 
 function App() {
   const [message, setMessage] = React.useState(null);
+  const [previewImg, setPreviewImg] = React.useState(undefined);
+  const [submitImg, setSubmitImg] = React.useState(undefined);
 
   React.useEffect(()=>{
     fetch('/api/getImage')
     .then((res) => res.json())
     .then((data) => setMessage(data.message));
   })
+
+  React.useEffect(() => {
+    if (!submitImg) {
+      setPreviewImg(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(submitImg);
+    setPreviewImg(objectUrl);
+
+    // free memory when ever this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [submitImg]);
+
+
+  const onSelectFile = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setSubmitImg(undefined);
+      return;
+    }
+
+    // I've kept this example simple by using the first image instead of multiple
+    setSubmitImg(e.target.files[0]);
+  };
+
+  const submitForm = () => {
+    // const imageForm = new FormData();
+    // imageForm.append("file", submitImg);
+    var requestOptions = {
+      method: "POST",
+      redirect: "manual",
+    };
+    fetch("/api/postImage", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         {message && <p>This is the message from the Backend: {message}</p>}
+        <form>
+          <input
+            type="file"
+            id="imageTBD"
+            name="imageTBD"
+            accept="image/png, image/jpeg"
+            onChange={onSelectFile}
+          />
+          <button onClick={submitForm}>Submit</button>
+        </form>
+
+        {submitImg && <img src={previewImg} />}
       </header>
     </div>
   );
--- server/controllers/ImageStorage-Controller.js
+++ server/controllers/ImageStorage-Controller.js
@@ -6,6 +6,7 @@ const getImage = async (req, res, next) => {
     res.json({ message: "Get Image!" });
 };
 const postImage = async (req, res, next) => {
+    console.log("Here")
     res.json({ message: "Post Image!" });
 };
 module.exports.getImage = getImage;
